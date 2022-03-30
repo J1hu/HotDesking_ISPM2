f507d4012c47ffa7ff18bea88a3c35bb8fdcbd44
malarosa <71916719+malarosa@users.noreply.github.com>
2022-03-30 18:31:29 +0800

    added footer

--- resources/views/layouts/include/footer.blade.php
+++ resources/views/layouts/include/footer.blade.php
@@ -1,1 +1,100 @@
-<h3>I am a footer</h3>
+<footer class="page-footer font-small blue pt-4">
+
+<!-- Footer Links -->
+<div class="container-fluid text-center text-md-left">
+
+  <!-- Grid row -->
+  <div class="row">
+
+    <!-- Grid column -->
+    <div class="col-md-6 mt-md-0 mt-3">
+
+      <!-- Content -->
+      <h5 class="text-uppercase"><b>ホットデスク Hot Desk</b></h5>
+      <p>Hot Desk : A user Friendly Workspace Scheduling Software.</p>
+
+      <section class="ml-4">
+      <!-- Facebook -->
+      <a
+        class="btn btn-primary btn-floating m-1"
+        style="background-color: #3b5998;"
+        href="#!"
+        role="button"
+        ><i class="fab fa-facebook-f"></i
+      ></a>
+
+       <!-- Instagram -->
+       <a
+       class="btn btn-primary btn-floating m-1"
+       style="background-color: #ac2bac;"
+       href="#!"
+       role="button"
+       ><i class="fab fa-instagram"></i
+     ></a>
+
+      <!-- Twitter -->
+      <a
+        class="btn btn-primary btn-floating m-1"
+        style="background-color: #55acee;"
+        href="#!"
+        role="button"
+        ><i class="fab fa-twitter"></i
+      ></a>
+    </section>
+
+    </div>
+    <!-- Grid column -->
+
+    <hr class="clearfix w-100 d-md-none pb-3">
+
+    <!-- Grid column -->
+    <div class="col-md-3 mb-md-0 mb-3">
+
+      <!-- Links -->
+      <h5 class="text-uppercase"><b>Overview</b></h5>
+
+      <ul class="list-unstyled">
+        <li>
+            Features
+        </li>
+        <li>
+            Tutorials
+        </li>
+      </ul>
+
+    </div>
+    <!-- Grid column -->
+
+    <!-- Grid column -->
+    <div class="col-md-3 mb-md-0 mb-3">
+
+      <!-- Links -->
+      <h5 class="text-uppercase"><b>Get Help</b></h5>
+
+      <ul class="list-unstyled">
+        <li>
+            Support
+        </li>
+        <li>
+            Help Center
+        </li>
+        <li>
+            Privacy Disclaimers
+        </li>
+      </ul>
+
+    </div>
+    <!-- Grid column -->
+
+  </div>
+  <!-- Grid row -->
+
+</div>
+<!-- Footer Links -->
+
+<!-- Copyright -->
+<div class="footer-copyright text-center py-3">© Copyright 2022 Group 1
+</div>
+<!-- Copyright -->
+
+</footer>
