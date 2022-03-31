1a057d94ca3a30210268cb5635e9a0e0e3e0c0c2
Justine Macabuhay <justinemacabuhay@student.laverdad.edu.ph>
2022-03-31 22:52:42 +0800

    footer changes

--- resources/views/layouts/include/footer.blade.php
+++ resources/views/layouts/include/footer.blade.php
@@ -1,91 +1,95 @@
-<footer class="page-footer font-small blue pt-4">
+<footer class="page-footer font-small blue pt-10" style="background-color: #27231E;">
 
 <!-- Footer Links -->
 <div class="container-fluid text-center text-md-left">
 
   <!-- Grid row -->
   <div class="row">
+    <div class="d-flex justify-content-between">
 
-    <!-- Grid column -->
-    <div class="col-md-6 mt-md-0 mt-3">
+      <!-- Grid column -->
+      <div class="col-md-3 mt-md-0 mt-2">
 
-      <!-- Content -->
-      <h5 class="text-uppercase"><b>ホットデスク Hot Desk</b></h5>
-      <p>Hot Desk : A user Friendly Workspace Scheduling Software.</p>
+        <!-- Content -->
+        <h5 class="text-uppercase mt-5 text-white"><b>ホットデスク Hot Desk</b></h5>
+        <p class="text-white">Hot Desk: A user Friendly <br>
+        Workspace Scheduling<br>
+        Software.</p>
 
-      <section class="ml-4">
-      <!-- Facebook -->
-      <a
+        <section class="ml-4">
+        <!-- Facebook -->
+        <a
+          class="btn btn-primary btn-floating m-1"
+          style="background-color: #3b5998;"
+          href="#!"
+          role="button"
+          ><i class="fab fa-facebook-f"></i
+        ></a>
+
+        <!-- Instagram -->
+        <a
         class="btn btn-primary btn-floating m-1"
-        style="background-color: #3b5998;"
+        style="background-color: #ac2bac;"
         href="#!"
         role="button"
-        ><i class="fab fa-facebook-f"></i
+        ><i class="fab fa-instagram"></i
       ></a>
 
-       <!-- Instagram -->
-       <a
-       class="btn btn-primary btn-floating m-1"
-       style="background-color: #ac2bac;"
-       href="#!"
-       role="button"
-       ><i class="fab fa-instagram"></i
-     ></a>
+        <!-- Twitter -->
+        <a
+          class="btn btn-primary btn-floating m-1"
+          style="background-color: #55acee;"
+          href="#!"
+          role="button"
+          ><i class="fab fa-twitter"></i
+        ></a>
+      </section>
 
-      <!-- Twitter -->
-      <a
-        class="btn btn-primary btn-floating m-1"
-        style="background-color: #55acee;"
-        href="#!"
-        role="button"
-        ><i class="fab fa-twitter"></i
-      ></a>
-    </section>
+      </div>
+      <!-- Grid column -->
 
+      <hr class="clearfix w-100 d-md-none pb-3">
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3">
+
+        <!-- Links -->
+        <h5 class="text-uppercase mt-5 text-white"><b>Overview</b></h5>
+
+        <ul class="list-unstyled text-white">
+          <li>
+              Features
+          </li>
+          <li>
+              Tutorials
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-3 mb-md-0 mb-3">
+
+        <!-- Links -->
+        <h5 class="text-uppercase mt-5 text-white" ><b>Get Help</b></h5>
+
+        <ul class="list-unstyled text-white">
+          <li>
+              Support 
+          </li>
+          <br>
+          <li>
+              Help Center
+          </li>
+          <li>
+              Privacy Disclaimers
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
     </div>
-    <!-- Grid column -->
-
-    <hr class="clearfix w-100 d-md-none pb-3">
-
-    <!-- Grid column -->
-    <div class="col-md-3 mb-md-0 mb-3">
-
-      <!-- Links -->
-      <h5 class="text-uppercase"><b>Overview</b></h5>
-
-      <ul class="list-unstyled">
-        <li>
-            Features
-        </li>
-        <li>
-            Tutorials
-        </li>
-      </ul>
-
-    </div>
-    <!-- Grid column -->
-
-    <!-- Grid column -->
-    <div class="col-md-3 mb-md-0 mb-3">
-
-      <!-- Links -->
-      <h5 class="text-uppercase"><b>Get Help</b></h5>
-
-      <ul class="list-unstyled">
-        <li>
-            Support
-        </li>
-        <li>
-            Help Center
-        </li>
-        <li>
-            Privacy Disclaimers
-        </li>
-      </ul>
-
-    </div>
-    <!-- Grid column -->
-
   </div>
   <!-- Grid row -->
 
