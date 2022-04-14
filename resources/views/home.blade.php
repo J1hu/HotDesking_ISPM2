7e0263a9c8ad4d3910e14bbd6c5de09a32d913af
John Kenneth Rosales <johnkennethrosales@student.laverdad.edu.ph>
2022-04-14 16:53:18 +0800

    added dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,38 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
+    <h1 class="text-left pt-5 display-4"><strong>Dashboard</strong></h2>
+    <p>Wed feb 23,2022</p>
+    <div class="container pb-5">
+        <div class="row">
+          <div class="col-sm">
+            <div class="card" style="width: 18rem;">
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                  <h5 class="text-right">Occupied</h5>
+                  <img src="{{URL('images/occupied.png') }}" class="figure-img img-fluid rounded">
                 </div>
             </div>
+          </div>
+          <div class="col-sm">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="text-right">Reserved</h5>
+                    <img src="{{URL('images/reserved.png') }}" class="figure-img img-fluid rounded">
+                </div>
+            </div>
+          </div>
+          <div class="col-sm">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="text-right">Available</h5>
+                    <img src="{{URL('images/available.png') }}" class="figure-img img-fluid rounded">
+                </div>
+            </div>
+          </div>
         </div>
-    </div>
+      </div>
+
+
+
 </div>
 @endsection
